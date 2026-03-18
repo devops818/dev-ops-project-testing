1b20d829881f0a225e31caeecd702647398afd45
devops818 <poormeintheworld@gmail.com>
2026-03-18 14:08:57 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,15 @@
-def buildApp() {
-  echo 'building the application...'
+def buildJar() {
+  echo "building the application"
+  sh 'mvn package'
+}
+
+def buildImage() {
+  echo "building the docker image"
+  withCredentials([usernamePassword(credentialsId: 'DockerHub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+    sh 'docker build -t janetdevop/demo-app:jma-2.0 .'
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push janetdevop/demo-app:jma-2.0'
+  }
 }
 
 def testApp() {
@@ -8,7 +18,6 @@ def testApp() {
 
 def deployApp() {
   echo 'deploying the application...'
-  echo "deploying version ${params.VERSION}"
 }
 
 return this
