524e5b7c8067d10101c82f576268c009482001d0
devops818 <poormeintheworld@gmail.com>
2026-03-26 16:52:25 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,12 +7,20 @@ def buildJar() {
   sh 'mvn package'
 }
 
+def incrementVersion() {
+  echo 'incrementing app version...'
+  sh 'mvn build-helper:parse-version versions:set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit'
+  def matcher = readFile('pom.xml') =~ '<version>(.+)</version>
+  def version = matcher[0][1]
+  env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
+
 def buildImage() {
   echo "building the docker image"
   withCredentials([usernamePassword(credentialsId: 'DockerHub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-    sh 'docker build -t janetdevop/demo-app:jma-2.0 .'
+    sh 'docker build -t janetdevop/demo-app:$IMAGE_NAME .'
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push janetdevop/demo-app:jma-2.0'
+    sh 'docker push janetdevop/demo-app:$IMAGE_NAME'
   }
 }
 
