9b6b358e55abb5ab2fc0bef50eb28d650984bb23
devops818 <poormeintheworld@gmail.com>
2026-04-20 15:36:49 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -42,13 +42,13 @@ def buildImage() {
 
 def deployApp() {
   echo 'deploying the docker to EC2...'
-  // def dockerCmd = 'docker run -p 3080:8080 -d janetdevop/demo-app:jma-${IMAGE_NAME}'
+  // def dockerCmd = 'docker run -p 8080:8080 -d janetdevop/demo-app:jma-${IMAGE_NAME}'
   // def dockerComposeCmd = "docker-compose -f /home/ec2-user/docker-compose.yaml up --detach"
   def shellCmd = "bash ./server-cmd.sh"
   sshagent(['ec2-key']) {
-    sh "scp docker-compose.yaml ec2-user@98.82.113.126:/home/ec2-user"
-    sh "scp server-cmd.sh ec2-user@98.82.113.126:/home/ec2-user"
-    sh "ssh -o StrictHostKeyChecking=no ec2-user@98.82.113.126 ${shellCmd}"
+    sh "scp docker-compose.yaml ec2-user@35.168.36.104:/home/ec2-user"
+    sh "scp server-cmd.sh ec2-user@35.168.36.104:/home/ec2-user"
+    sh "ssh -o StrictHostKeyChecking=no ec2-user@35.168.36.104 ${shellCmd}"
   }
 }
 
